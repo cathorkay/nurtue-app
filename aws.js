241cc2f39270b07d847e019bb4a8a854df59d01e
Michael <michaelding2233@gmail.com>
2023-12-31 10:37:08 -0800

    removal of AWS keys, can't be public like this

--- aws.js
+++ aws.js
@@ -1,7 +1,7 @@
 const BUCKET_NAME = 'nurtue-images';
 const REGION = 'us-west-1';
-const ACCESSKEYID = 'AKIARHZIOEFA2QK53RTX';
-const SECRETACCESSKEY = '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';
+const ACCESSKEYID = '';
+const SECRETACCESSKEY = '';
 
 export { BUCKET_NAME, REGION, ACCESSKEYID, SECRETACCESSKEY };
   
